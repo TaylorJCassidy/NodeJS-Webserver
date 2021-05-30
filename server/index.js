2c41cee444cd7e28a4e4ffbad418bfb755ecb6ae
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-31 00:47:05 +0100

    added main webpage controller

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const {host,port} = require('./configs/hostandport.json');
+const webfiles = require('./utils/getWebfiles').getWebfiles();
 
-const server = http.createServer((req,res) => {
-
+http.createServer((req,res) => {
+    try {
+        const webfile = webfiles.get(req.url);
+        res.writeHead(200,webfile.header);
+        res.end(webfile.content)
+    }
+    catch(e) {
+        res.writeHead(404,'There has been an internal error');
+        res.end();
+    } 
 }).listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
